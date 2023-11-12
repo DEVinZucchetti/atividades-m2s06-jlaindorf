bc11b2651faf4dd4f0ff5cdc0eb49bccacf9c63a
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-11-12 19:45:02 -0300

    cria store

--- M2S06/app/Http/Controllers/PessoasController.php
+++ M2S06/app/Http/Controllers/PessoasController.php
@@ -17,7 +17,21 @@ class PessoasController extends Controller
         } catch(\Exception $exception){
            return $this->response($exception->getMessage(), null, false, 500);
         }
+    }
 
+    public function store(Request $request){
+        try{
+            $request->validate([
+                'name' => 'required | min: 3 | max: 150',
+                'cpf' => 'min: 11 | max: 20',
+                'contact' => 'max: 20',
+            ]);
 
+            $pessoa = Pessoa::create($request->all());
+            $message = $pessoa->name." cadastrada com sucesso";
+            return $this->response($message, $pessoa);
+        }catch(\Exception $exception){
+           return $this->response($exception->getMessage(), null, false, 500);
+        }
     }
 }
--- M2S06/routes/api.php
+++ M2S06/routes/api.php
@@ -8,5 +8,6 @@ use App\Http\Controllers\PessoasController;
 Route::prefix('pessoas')->group(function(){
 
     Route::get('',[PessoasController::class, 'all']);
+    Route::post('',[PessoasController::class, 'store']);
 
 });
